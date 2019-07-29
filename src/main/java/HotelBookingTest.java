54e6697022a5f746abab921b7422aaf045f34a26
netraa <42333860+netraa@users.noreply.github.com>
2019-07-29 17:30:43 +0530

    Removed unnecessary code & imports and added clean working code to the HitelBooking class

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,49 +1,54 @@
 import com.sun.javafx.PlatformUtil;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
-
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
-
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
-
+	 WebDriver driver;
+	 
+		   
+	 public HotelBookingTest(WebDriver driver){ 
+         this.driver=driver; 
+}
+    
+     
     @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+    public void shouldBeAbleToSearchForHotels() throws InterruptedException {
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
-
-        driver.quit();
-
+    	waitFor(2000);
+//        driver.get("https://www.cleartrip.com/");
+    	
+    
+      driver.findElement(By.xpath("//section[@id='GlobalNav']/div/div[2]/div/strong")).click();
+      driver.findElement(By.xpath("//[@class='ctBrand']")).click();
+      driver.findElement(By.xpath("//a[contains(text(),'Hotels')]")).click();
+      driver.findElement(By.xpath("//input[@id='Tags']")).click();
+      driver.findElement(By.xpath("//input[@id='Tags']")).sendKeys("Indiranagar, Bangalore");
+      driver.findElement(By.id("SearchHotelsButton")).click();
+      
+        
+    }
+    
+    private void waitFor(int durationInSeconds) {
+        try {
+           // Thread.sleep(durationInMilliSeconds);
+            driver.manage().timeouts().implicitlyWait(durationInSeconds, TimeUnit.SECONDS);	
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
-    private void setDriverPath() {
+    @SuppressWarnings("restriction")
+	private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
         if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        	System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
