5f89cec4a5744a992f40772840501308194edc92
netraa <42333860+netraa@users.noreply.github.com>
2019-07-29 17:31:45 +0530

    Sign In class is updated with working code & removed unnecessary code & imports

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,51 +1,47 @@
-import com.sun.javafx.PlatformUtil;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
-
-    WebDriver driver = new ChromeDriver();
-
+	WebDriver driver;
+	 
+    public SignInTest(WebDriver driver){ 
+             this.driver=driver; 
+    }
+   
     @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+    public void shouldThrowAnErrorIfSignInDetailsAreMissing() throws InterruptedException {
 
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-
+    	/*setDriverPath();
+    	driver = new ChromeDriver();
+        driver.get("https://www.cleartrip.com/");*/
+        
+    	//calling implicit wait for 2 sec
+    	waitFor(2000);
+    	
+    	//Clicking on the Your Trip which is located at right top corner
         driver.findElement(By.linkText("Your trips")).click();
+        
+      //Clicking on the Sign-in option in the Your Trip drop down
         driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
+        
+    
+      //Catching the error message through assert
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }
 
-    private void waitFor(int durationInMilliSeconds) {
+    private void waitFor(int durationInSeconds) {
         try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
+           // Thread.sleep(durationInMilliSeconds);
+            driver.manage().timeouts().implicitlyWait(durationInSeconds, TimeUnit.SECONDS);	
+        } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
 
 }
