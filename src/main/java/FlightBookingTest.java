1c0c3dfb0ce301316f2cb903e47826f77e45bb44
netraa <42333860+netraa@users.noreply.github.com>
2019-07-29 17:29:31 +0530

    Removed unnecessary code & imports & removed errors form FlightBooking class

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,68 +1,78 @@
-import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FlightBookingTest {
-
-    WebDriver driver = new ChromeDriver();
-
-
+	WebDriver driver;
+	 
+    public FlightBookingTest(WebDriver driver){ 
+             this.driver=driver; 
+    }
+    
+    
     @Test
-    public void testThatResultsAppearForAOneWayJourney() {
-
-        setDriverPath();
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-        driver.findElement(By.id("OneWay")).click();
-
+    public void testThatResultsAppearForAOneWayJourney() throws InterruptedException {
+    	
+       /* setDriverPath();
+        driver.get("https://www.cleartrip.com/");*/
+    	
+    	//calling implicit wait for 2 sec
+    	waitFor(2000);
+       
+    	//clicking on the one way (Anyway it is default selected. if not one way click code written also following code work)
+    	driver.findElement(By.id("OneWay")).click();
+    	
+    	//Selecting Source place
         driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        driver.findElement(By.id("FromTag")).sendKeys("Bangalore"); //Sending data in Source place
 
         //wait for the auto complete options to appear for the origin
 
         waitFor(2000);
-        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
-        originOptions.get(0).click();
+//      List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+//      originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        //Selecting Destination place
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
-        //wait for the auto complete options to appear for the destination
+      //wait for the auto complete options to appear for the origin
 
         waitFor(2000);
-        //select the first item from the destination auto complete list
+       //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        //Selecting Current Date as Departed Date
+        driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active ")).click();
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+       // waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
-        driver.quit();
+        //driver.quit();
+        
 
     }
 
 
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+    private void waitFor(int durationInSeconds) {
+    	try {
+            // Thread.sleep(durationInMilliSeconds);
+             driver.manage().timeouts().implicitlyWait(durationInSeconds, TimeUnit.SECONDS);	
+         } catch (Exception e) {
+             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+         }
     }
 
 
@@ -75,15 +85,5 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+   
 }
